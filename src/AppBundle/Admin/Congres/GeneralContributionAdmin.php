29754728822397a56de0b00f7600314c16ebc7c6
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2015-01-14 01:44:32 +0100

    Removed ability to create contribution form the sonata admin.

--- src/AppBundle/Admin/Congres/GeneralContributionAdmin.php
+++ src/AppBundle/Admin/Congres/GeneralContributionAdmin.php
@@ -6,9 +6,15 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBUndle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 
 class GeneralContributionAdmin extends ContributionAdmin
 {
     protected $baseRouteName = 'congres/contributions/generales';
     protected $baseRoutePattern = 'congres/contributions/generales';
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+    }
 }
--- src/AppBundle/Admin/Congres/ThematicContributionAdmin.php
+++ src/AppBundle/Admin/Congres/ThematicContributionAdmin.php
@@ -7,9 +7,15 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBUndle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 
 class ThematicContributionAdmin extends ContributionAdmin
 {
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+    }
+
     protected $baseRouteName = 'congres/contributions/generales';
     protected $baseRoutePattern = 'congres/contributions/generales';
 }
