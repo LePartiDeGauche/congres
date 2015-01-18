9031d20222884db03cfd80d29460f9ced87f8f61
Nael Ferret <naelferret@gmail.com>
2015-01-19 00:22:08 +0100

    fix wrong status in contribution voter vote

--- src/AppBundle/Security/Congres/ContributionVoter.php
+++ src/AppBundle/Security/Congres/ContributionVoter.php
@@ -50,7 +50,7 @@ final class ContributionVoter extends AbstractVoter
         }
 
         if ($attribute === self::VOTE) {
-            if (Contribution::STATUS_SIGNATURES_OPEN !== $contrib->getStatus()) {
+            if (Contribution::STATUS_SIGNATURES_OPEN == $contrib->getStatus()) {
                 $em = $this->entityManager;
 
                 if (is_a($contrib, 'AppBundle\Entity\Congres\GeneralContribution')) {
