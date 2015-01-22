d11ba35c23dc0d97d6c84dec6c56b7876d8a42d6
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2015-01-23 00:57:59 +0100

    added a homepage, minor ui changes

---
+++ src/AppBundle/Controller/AdherentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdherentController extends Controller
+{
+    /**
+     * @Route("/mon-compte/profil", name="adherent_profile")
+     */
+    public function profileAction()
+    {
+        return $this->render('adherent/profile.html.twig');
+    }
+
+}
