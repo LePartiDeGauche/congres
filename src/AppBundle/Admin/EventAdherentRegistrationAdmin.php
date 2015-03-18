2edd76765adde4fbb766f9616d1c5bd7f76802d4
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2015-03-18 01:55:32 +0100

    EventAdherentAdmin

---
+++ src/AppBundle/Admin/EventAdherentRegistrationAdmin.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class EventAdherentRegistrationAdmin extends Admin
+{
+    protected $baseRouteName = 'event_registration_admin';
+    protected $baseRoutePattern = 'event/registration';
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('needHosting')
+            ->add('paymentMode')
+            ->add('registrationDate')
+            ->add('comment')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('needHosting')
+            ->add('paymentMode')
+            ->add('registrationDate')
+            ->add('comment')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('needHosting')
+            ->add('paymentMode')
+            ->add('registrationDate')
+            ->add('comment')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('needHosting')
+            ->add('paymentMode')
+            ->add('registrationDate')
+            ->add('comment')
+        ;
+    }
+}
