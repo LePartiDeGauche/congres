e041ef87a1a96ef0172f6240b33a0200dc1043bf
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2015-04-19 16:55:17 +0200

    textgroup voter refactoring and WIP report_vote rights

--- src/AppBundle/Security/Text/TextGroupVoter.php
+++ src/AppBundle/Security/Text/TextGroupVoter.php
@@ -11,15 +11,16 @@ final class TextGroupVoter extends AbstractVoter
 
     const CREATE_TEXT = 'create_text';
 
-
     const VOTE = 'vote';
+
+    const REPORT_VOTE = 'report_vote';
 
     private $entityManager;
     private $container;
 
     protected function getSupportedAttributes()
     {
-        return array(self::VIEW, self::CREATE_TEXT, self::VOTE);
+        return array(self::VIEW, self::CREATE_TEXT, self::VOTE, self::REPORT_VOTE);
     }
 
     protected function getSupportedClasses()
@@ -27,7 +28,7 @@ final class TextGroupVoter extends AbstractVoter
         return array('AppBundle\Entity\Text\TextGroup');
     }
 
-    protected function isGranted($attribute, $textGroup, $user = null)
+    protected function isGranted($attribute, $textGroup, $user = null, $organ = null)
     {
         $em = $this->entityManager;
         if (!$user instanceof UserInterface) {
@@ -36,31 +37,79 @@ final class TextGroupVoter extends AbstractVoter
         $date = new \DateTime('now');
 
         if ($attribute === self::VIEW) {
-            return ($textGroup->getIsVisible() ||
-                in_array('ROLE_ADMIN', $user->getRoles(), true));
+            return $this->canView($textGroup, $user);
         }
 
         if ($attribute === self::CREATE_TEXT) {
+            return $this->canCreateText($textGroup, $user);
 
-            return ($textGroup->getIsVisible() && $textGroup->getSubmissionOpening() < $date && $textGroup->getSubmissionClosing() > $date) ||
-                in_array('ROLE_ADMIN', $user->getRoles(), true);
         }
 
         if ($attribute === self::VOTE) {
-            $em = $this->entityManager;
+            return $this->canCreateText($textGroup, $user);
+        }
 
-            if ($textGroup->getIsVisible() && $textGroup->getVoteOpening() < $date && $textGroup->getVoteClosing() > $date)
-            {
-                return $em->getRepository('AppBundle:Vote\IndividualTextVote')
-                    ->getAdherentVoteCountByTextGroup($user->getProfile(), $textGroup) < 
-                    $textGroup->getMaxVotesByAdherent() &&
-                    $em->getRepository('AppBundle:Vote\VoteRule')
-                    ->getAdherentRightToVoteForTextGroup($user->getProfile(), $textGroup);
-            }
+        if ($attribute === self::REPORT_VOTE) {
+            return $this->canReportVote($textGroup, $user, $organ);
         }
 
         return false;
     }
+
+    private function canView (TextGroup $textGroup,User $user)
+    {
+        // FIXME : in_array does not take into account role hierarchy !
+        return ($textGroup->getIsVisible() ||
+            in_array('ROLE_ADMIN', $user->getRoles(), true));
+    }
+
+    private function canCreateText (TextGroup $textGroup, User $user)
+    {
+        $date = new \DateTime('now');
+        // FIXME : in_array does not take into account role hierarchy !
+        return ($textGroup->getIsVisible() && $textGroup->getSubmissionOpening() < $date && $textGroup->getSubmissionClosing() > $date) ||
+            in_array('ROLE_ADMIN', $user->getRoles(), true);
+
+    }
+
+    private function canVote (TextGroup $textGroup, User $user)
+    {
+        $date = new \DateTime('now');
+        $em = $this->entityManager;
+
+        if ($textGroup->getIsVisible() && $textGroup->getVoteOpening() < $date && $textGroup->getVoteClosing() > $date)
+        {
+            return $em->getRepository('AppBundle:Vote\IndividualTextVote')
+                ->getAdherentVoteCountByTextGroup($user->getProfile(), $textGroup) < 
+                $textGroup->getMaxVotesByAdherent() &&
+                $em->getRepository('AppBundle:Vote\VoteRule')
+                ->getAdherentRightToVoteForTextGroup($user->getProfile(), $textGroup);
+        }
+        return false;
+    }
+
+    /*
+     * a adherent can report a vote if :
+     * - text Group is open for vote (done)
+     * - the organ is abilitated to vote. (done)
+     * - the organ has not voted yet.
+     * - the adherent as as the right responsability to vote.
+     */
+    private function canReportVote (TextGroup $textGroup, User $user, Organ $organ)
+    {
+        $date = new \DateTime('now');
+        $em = $this->entityManager;
+
+        if ($textGroup->getIsVisible() && $textGroup->getVoteOpening() < $date && $textGroup->getVoteClosing() > $date)
+        {
+            return
+                $em->getRepository('AppBundle:Vote\OrganVoteRule')->getOrganTypeRightToVoteForTextGroup($organ->organType);
+
+        }
+
+        return false;
+    }
+
 
     public function __construct($entityManager, $container)
     {
