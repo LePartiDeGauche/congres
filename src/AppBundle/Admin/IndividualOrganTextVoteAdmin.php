cac85c938b83b07b7a67503bc17610458e8e962c
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2015-04-24 02:58:36 +0200

    beginning organ vote admin

---
+++ src/AppBundle/Admin/IndividualOrganTextVoteAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class IndividualOrganTextVoteAdmin extends Admin
+{
+    protected $baseRouteName = 'text_vote_report_admin';
+    protected $baseRoutePattern = 'text/vote/report';
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('voteTotal')
+            ->add('voteAbstention')
+            ->add('voteNotTakingPart')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('author.firstname')
+            ->add('author.lastname')
+            ->add('organ.name')
+            ->add('voteTotal')
+            ->add('voteAbstention')
+            ->add('voteNotTakingPart')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('voteTotal')
+            ->add('voteAbstention')
+            ->add('voteNotTakingPart')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('voteTotal')
+            ->add('voteAbstention')
+            ->add('voteNotTakingPart')
+        ;
+    }
+}
