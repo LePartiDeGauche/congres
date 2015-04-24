263996113080c5cbaca01d9cffd723506b2b037f
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2015-04-24 20:10:23 +0200

    added admin

---
+++ src/AppBundle/Admin/IndividualOrganTextVoteAgregationAdmin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class IndividualOrganTextVoteAgregationAdmin extends Admin
+{
+    protected $baseRouteName = 'text_vote_report_agreg_admin';
+    protected $baseRoutePattern = 'text/vote/report_agreg';
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+        $collection->remove('edit');
+    }
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('voteFor')
+            ->add('voteAgainst')
+            ->add('voteAbstention')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('voteFor')
+            ->add('voteAgainst')
+            ->add('voteAbstention')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('voteFor')
+            ->add('voteAgainst')
+            ->add('voteAbstention')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('voteFor')
+            ->add('voteAgainst')
+            ->add('voteAbstention')
+        ;
+    }
+}
