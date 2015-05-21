b3169d945ce998e824a7d54ade1f40f36f6972f3
clement <clement@la-cooperative-des-tilleuls.com>
2015-05-21 10:47:41 +0200

    [wip]getters and setters in entities

--- src/AppBundle/Entity/Election/Delegate.php
+++ src/AppBundle/Entity/Election/Delegate.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Entity\Congres;
 
+use AppBundle\Entity\Adherent;
+use AppBundle\Entity\Organ\Organ;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -27,7 +29,7 @@ class Delegate
      * The delegate.
      *
      * @var \AppBundle\Entity\Adherent
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Adherent")
      */
     private $adherent;
 
@@ -38,4 +40,59 @@ class Delegate
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Organ\Organ")
      */
     private $organ;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set id.
+     *
+     * @return int
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return Adherent
+     */
+    public function getAdherent()
+    {
+        return $this->$adherent;
+    }
+
+    /**
+     * @param Adherent $adherent
+     */
+    public function setAdherent(Adherent $adherent)
+    {
+        $this->adherent = $adherent;
+    }
+
+    /**
+     * @return Organ
+     */
+    public function getOrgan(Organ $organ)
+    {
+        return $this->$organ;
+    }
+
+    /**
+     * @param Organ $organ
+     */
+    public function setOrgan(Organ $organ)
+    {
+        $this->organ = $organ;
+    }
 }
