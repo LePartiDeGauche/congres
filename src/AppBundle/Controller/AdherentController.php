df4fd055234d78ed0cf2bc383e7859414a6d3705
clement <clement@la-cooperative-des-tilleuls.com>
2015-05-29 13:03:50 +0200

    [wip]auto complete #1

--- src/AppBundle/Controller/AdherentController.php
+++ src/AppBundle/Controller/AdherentController.php
@@ -2,9 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Adherent controller.
+ *
+ * @Route("/adherents")
+ */
 class AdherentController extends Controller
 {
     /**
@@ -13,5 +20,20 @@ class AdherentController extends Controller
     public function profileAction()
     {
         return $this->render('adherent/profile.html.twig');
+    }
+
+    /**
+     * Search adherents.
+     *
+     * @Route("/search_adherent/{typedText}", name="search_adherent")
+     * @Method({"GET"})
+     *
+     * @param $typedText
+     *
+     * @return JsonResponse
+     */
+    public function searchAdherentAction($typedText = '')
+    {
+        return new JsonResponse($this->getDoctrine()->getRepository('AppBundle:Adherent')->findAdherentsByTypedText($typedText));
     }
 }
