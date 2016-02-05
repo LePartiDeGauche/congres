ab8e9bf01ee427d9ce72999cb49668b03bd4c8d8
mcbennn <mcbennn@gmail.com>
2016-02-05 14:32:57 +0100

    Fixes last migration

--- app/DoctrineMigrations/Version20160205131039.php
+++ app/DoctrineMigrations/Version20160205131039.php
@@ -18,7 +18,6 @@ class Version20160205131039 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE AmendmentTopic');
         $this->addSql('ALTER TABLE amendment_process ADD isVisible TINYINT(1) NOT NULL');
     }
 
@@ -30,7 +29,6 @@ class Version20160205131039 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE AmendmentTopic (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, description LONGTEXT NOT NULL COLLATE utf8_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE amendment_process DROP isVisible');
     }
 }
