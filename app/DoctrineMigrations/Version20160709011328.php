c332197cc2e312cc2a4ab4bfe4b249aa31625888
mcbennn <mcbennn@gmail.com>
2016-07-09 01:52:14 +0200

    Doctrine migration for vegan meals

---
+++ app/DoctrineMigrations/Version20160709011328.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160709011328 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event ADD is_vegan_meals_enabled TINYINT(1) DEFAULT \'0\' NOT NULL');
+        $this->addSql('ALTER TABLE EventAdherentRegistration ADD is_vegan_meals_preferred TINYINT(1) DEFAULT \'0\' NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE EventAdherentRegistration DROP is_vegan_meals_preferred');
+        $this->addSql('ALTER TABLE event DROP is_vegan_meals_enabled');
+    }
+}
